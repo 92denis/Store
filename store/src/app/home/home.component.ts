7ede633801648ee93bb5d96a2ca24698957915c2
Denis Sadowskiy <92denis-s@mail.ru>
2017-09-20 20:56:41 +0300

    HomeComponent: reset form

--- store/src/app/home/home.component.ts
+++ store/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '../store';
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
+import { NgForm} from '@angular/forms';
 
 
 @Component({
